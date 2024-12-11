39c1dcbad323ee63c8ae6fdc3805c21154f95e7b
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-11 13:31:42 +0530

    Add files via upload

---
+++ staticnonvoidmethod.java
@@ -0,0 +1,10 @@
+class e
+{
+    public static String s(){
+        return "Hello student";
+    }
+    public static void main(String args[]){
+        String res =s();
+        System.out.println(res);
+    }
+}
